d70bb6adcb53571b3a2bb2241f3aea242b3c4c9f
quder0210 <quder0210@naver.com>
2023-08-24 20:37:40 +0900

    test_update
    테스트입니다.

--- src/app/lecture/page.tsx
+++ src/app/lecture/page.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Lecture/Lecture.module.css'
 export default function Page() {
     return (
         <div className={styles.container}>
-            <h1>This is Lecture</h1>
+            <h1>This is Lecture입니다!!!!</h1>
         </div>
     )
 }
