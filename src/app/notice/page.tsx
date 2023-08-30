3c1978cc73be5dddee93e6196dca270c0a3e42e1
quder0210 <quder0210@naver.com>
2023-08-30 16:46:49 +0900

    test
    test

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -4,6 +4,10 @@ export default function Page() {
     return (
         <div className={styles.container}>
             <h1>This is Notice</h1>
+            <div className={styles.noticeBox}>
+                <h1>뭐가 어떻게 되고 있는건데</h1>
+                <h1>일단 마음에 안들어</h1>
+            </div>
         </div>
     )
 }
