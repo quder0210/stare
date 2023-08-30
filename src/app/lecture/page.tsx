cf75d7bfd205dfb4550a79da63f29771c6780693
quder0210 <quder0210@naver.com>
2023-08-30 15:15:19 +0900

    back
    back

--- src/app/lecture/page.tsx
+++ src/app/lecture/page.tsx
@@ -2,19 +2,8 @@ import styles from '@/styles/Lecture/Lecture.module.css'
 
 export default function Page() {
     return (
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>공지 페이지</title>
-        </head>
-        <body>
-            <div class="container">
-                <h1>중요한 공지사항</h1>
-                <p>이 페이지는 중요한 내용을 공지하기 위해 만들어졌습니다.</p>
-                <p>본문 내용이나 업데이트 사항에 대한 정보는 여기에 표시됩니다.</p>
-            </div>
-        </body>
-        </html>>
+        <div className={styles.container}>
+            <h1>This is Lecture</h1>
+        </div>
     )
 }
